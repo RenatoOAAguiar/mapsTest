570171bd4a8bf36d6843cba870d6a62c68476628
Renato Oliveira Alves Aguiar <renatooaa@outlook.com>
2019-01-04 17:34:47 -0200

    alterações do component maps

--- src/app/layout/maps/maps.component.ts
+++ src/app/layout/maps/maps.component.ts
@@ -8,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class MapsComponent implements OnInit {
 
   title: string = 'Meu primeiro projeto?';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: number = -16.6902082;
+  lng: number = -49.2759025;
   local = '';
+  zoom: number = 13;
+
+  markers = [
+	  {
+		  lat: -16.6902082,
+		  lng: -49.2759025,
+		  label: 'A',
+		  draggable: true
+	  },
+	  {
+		  lat: -16.7103527,
+		  lng: -49.3127187,
+		  label: 'B',
+		  draggable: false
+	  },
+	  {
+		  lat: -16.7076654,
+		  lng: -49.2753152,
+		  label: 'C',
+		  draggable: true
+	  }
+  ]
 
   constructor() { }
 
@@ -26,4 +48,9 @@ export class MapsComponent implements OnInit {
     }
   }
 
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+
 }
+
